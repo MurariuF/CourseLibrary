08a50a7e579b87648c4a290f66207d5b80f5a7cf
MurariuF <murariu.flavius@gmail.com>
2022-03-29 16:49:24 +0300

    Adding AutoMapper and configure it

---
+++ CourseLibrary/Profiles/AuthorsProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Profiles
+{
+    public class AuthorsProfile : Profile
+    {
+        public AuthorsProfile()
+        {
+            //Projecton destination name from source firstname and lastname
+            CreateMap<Entities.Author, Models.AuthorDto>()
+                .ForMember(
+                    dest => dest.Name,
+                    opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
+                .ForMember(
+                    dest => dest.Age,
+                    opt => opt.MapFrom(src => src.DateOfBirth.GetCurrentAge()));
+        }
+    }
+}
