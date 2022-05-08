3c9ff1d118cb9458c376520dddc0799be3380c00
MurariuF <murariu.flavius@gmail.com>
2022-05-08 04:12:11 +0300

    Added paginations

--- CourseLibrary/Services/CourseLibraryRepository.cs
+++ CourseLibrary/Services/CourseLibraryRepository.cs
@@ -1,5 +1,6 @@
 ﻿using CourseLibrary.API.DbContexts;
 using CourseLibrary.API.Entities;
+using CourseLibrary.API.Helpers;
 using CourseLibrary.API.ResourceParameters;
 using System;
 using System.Collections.Generic;
@@ -124,12 +125,11 @@ namespace CourseLibrary.API.Services
             return _context.Authors.ToList<Author>();
         }
 
-        public IEnumerable<Author> GetAuthors(AuthorsResourceParameters authorsResourceParameters)
+        public PagedList<Author> GetAuthors(AuthorsResourceParameters authorsResourceParameters)
         {
-            if(string.IsNullOrWhiteSpace(authorsResourceParameters.MainCategory)
-                && string.IsNullOrWhiteSpace(authorsResourceParameters.SearchQuery))
+            if (authorsResourceParameters == null)
             {
-                return GetAuthors();
+                throw new ArgumentNullException(nameof(authorsResourceParameters));
             }
 
             var collection = _context.Authors as IQueryable<Author>;
@@ -148,8 +148,9 @@ namespace CourseLibrary.API.Services
                 || a.LastName.Contains(searchQuery));
             }
 
-
-            return collection.ToList();
+            return PagedList<Author>.Create(collection,
+                authorsResourceParameters.PageNumber,
+                authorsResourceParameters.PageSize);
         }
 
         public IEnumerable<Author> GetAuthors(IEnumerable<Guid> authorIds)
